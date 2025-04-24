2c01de7b156ff479d1b48acaa9bc716413f7d51f
DannyMnt <munteanudaniel098@gmail.com>
2025-04-24 22:59:10 +0200

    changes

--- WEB2Asg2/src/main.jsx
+++ WEB2Asg2/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
 import PokemonList from './pages/PokemonList.jsx'
 import About from './pages/About.jsx'
 import PokemonInfo from './pages/PokemonInfo.jsx'
 import './index.css'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
